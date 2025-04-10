3ac523df5cdd9ec6413f956868b5d44f834156b9
Ghast <wolfsparkvanb@gmail.com>
2025-04-10 14:19:13 -0500

    paperwork beginning

---
+++ Content.Shared/_Funkystation/Paper/PaperworkPacketComponent.cs
@@ -0,0 +1,17 @@
+﻿using Content.Shared.Paper;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Funkystation.Paper;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed class PaperworkPacketComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public List<PaperComponent> Papers = new();
+
+    [DataField]
+    public int CurrentlySelected;
+
+    [DataField, AutoNetworkedField]
+    public PaperComponent? SelectedPaper;
+}
