b5e84d5407961933e04ffba29357996318cd9afd
Ghast <wolfsparkvanb@gmail.com>
2025-04-24 13:01:03 -0500

    mail starts

---
+++ Content.Shared/_Funkystation/Storage/LockedStorageComponent.cs
@@ -0,0 +1,10 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._Funkystation.Storage;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class LockedStorageComponent : Component
+{
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public bool ContainerLocked { get; set; }
+}
---
+++ Content.Shared/_Funkystation/Storage/LockedStorageSystem.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared.Storage;
+using Content.Shared.Storage.EntitySystems;
+
+namespace Content.Shared._Funkystation.Storage;
+
+public sealed class LockedStorageSystem : EntitySystem
+{
+    [Dependency] private readonly SharedStorageSystem _storage = default!;
+
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<LockedStorageComponent, StorageInteractAttemptEvent>(OnLockedStorageTransfer);
+    }
+
+    public void OnLockedStorageTransfer(Entity<LockedStorageComponent> component, ref StorageInteractAttemptEvent args)
+    {
+        if (component.Comp.ContainerLocked)
+        {
+            args.Cancelled = true;
+        }
+    }
+
+}
